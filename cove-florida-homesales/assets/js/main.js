122923bcce7de371c75ab97c65f7940b2895e017
Peter Sahlberg <pete@swimcreative.com>
2018-08-13 12:31:02 -0500

    updated js

--- cove-florida-homesales/assets/js/main.js
+++ cove-florida-homesales/assets/js/main.js
@@ -124,7 +124,7 @@ jQuery(function($) {
   cove.smoothScroll = function() {
 
     var height = $('header').outerHeight(),
-        link = $('a[href*="#"]:not(.burger):not(.item a):not(.gallery a):not(.close):not(.prev):not(.next)');
+        link = $('a[href*="#"]:not(.burger):not(.item a):not(.gallery a):not(.close):not(.prev):not(.next):not(#about .btn)');
 
     link.on('click', function(e) {
       e.preventDefault();
